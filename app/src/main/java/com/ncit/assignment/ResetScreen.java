5ec4ecda598dd48cff70094ba8fa90af425afffe
Rajan Paudel <rajan.rp50@gmail.com>
2021-12-10 20:25:02 +0545

    login screens

---
+++ app/src/main/java/com/ncit/assignment/ResetScreen.java
@@ -0,0 +1,14 @@
+package com.ncit.assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResetScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_screen);
+    }
+}
